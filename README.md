0c5645879b90bf2f8e10ce1c4ceaa87fc6af3301
Artyom Pervukhin <artyom@evasive.ru>
2017-09-09 13:38:23 +0300

    Add license and description

---
+++ README.md
@@ -0,0 +1,2 @@
+Command stem runs program in a linux container-like environment: chrooted to
+given directory and with separate pid and mount namespaces.
--- stem.go
+++ stem.go
@@ -1,5 +1,7 @@
 // +build linux
 
+// Command stem runs program in a linux container-like environment: chrooted to
+// given directory and with separate pid and mount namespaces.
 package main
 
 import (
